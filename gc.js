15a3023108711939bc497e91f480e926fff64ffd
Michael Kenneth Rasch <miketriticum@gmail.com>
2019-02-15 12:47:45 +0100

    Update gc.js

--- gc.js
+++ gc.js
@@ -7,14 +7,13 @@ function initClient() {
     clientId: '',
     discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
     scope: 'https://www.googleapis.com/auth/calendar.readonly',
-  }).then(function () {
+  }).then(function() {
     listUpcomingEvents()
-   }, function(error) {
+  }, function(error) {
     appendPre(JSON.stringify(error, null, 2), 'lColumn');
   });
 }
 function listUpcomingEvents() {
-  
   gapi.client.calendar.events.list({
     'calendarId': 'primary',
     'timeMin': (new Date()).toISOString(),
@@ -26,67 +25,62 @@ function listUpcomingEvents() {
     calendarEntriesUI(response);
   });
 }
-  function calendarEntriesUI (calendarJSON) { 
-    var gcSlots = JSON.parse(calendarJSON);
-    const oRadius = 500, iRadius = 400; 
-    var zOffset = 0, zMinutes = 0, jx=0, ix=0, zh=0, arcSweep = 0;
-    var zero = new Date(gcSlots.items[0].start.dateTime);    
-    var t = Date.now(); 
-    
-    var svg = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
-    svg.setAttribute("width", "100%");
-    svg.setAttribute("viewBox", "0 0 1200 1200");
-    svg.setAttribute("color", "#fff");
-    svg.setAttribute("background", "#000");
-    
-    zero.getHours() > 12 ? zh = zero.getHours() - 12 : zh = zero.getHours();
-    zOffset = 450 - ((zh * 30) + (zero.getMinutes() / 2));
-    zMinutes = t / 60000 / 2; 
-    (zMinutes > 180) ? arcSweep = 1 : arcSweep = 0; 
-    svg.appendChild(bCircle(500, "#212121"));   
-    svg.appendChild(pathPeriod(zOffset,zOffset - zMinutes,iRadius,oRadius,arcSweep,"#A00",500));             
-    svg.appendChild(bCircle(400, "#000"));   
-    svg.appendChild(addText(244, "A00", 500, 360, parseInt(zero / 60000), 500));
-    svg.appendChild(addText(80, "#888", 500, 492, zero,500));   
-    
-    return svg;
-
-    function addText (fType, fSize, color, x, y, title, fWeight) {      
-      var text = document.createElementNS("http://www.w3.org/2000/svg", 'text'); 
-      text.setAttribute("font-size",  fSize);  
-      text.setAttribute("font-weight",  fWeight);      
-      text.setAttribute("fill", color);      
-      text.setAttribute("x",  x);     
-      text.setAttribute("y",  y);           
-      text.textContent = title;
-      return text;
-    }
-    function pathPeriod (t0, t1, iRadius, oRadius, arcSweep, fill) {
-      var path = document.createElementNS("http://www.w3.org/2000/svg", 'path');
-      path.setAttribute("fill", fill);      
-      path.setAttribute("stroke-width", 0);  
-      path.setAttribute("d",
-        "M " + parseFloat(500 + (Math.cos(t1 * Math.PI/180) * iRadius)) 
-      + ", " + parseFloat(500 - (Math.sin(t1 * Math.PI/180) * iRadius))
-      + "L " + parseFloat(500 + (Math.cos(t1 * Math.PI/180) * oRadius)) 
-      + ", " + parseFloat(500 - (Math.sin(t1 * Math.PI/180) * oRadius))
-      + "A " + oRadius + "," + oRadius + " 0 " + arcSweep + " ,0 " 
-             + parseFloat(500 + (Math.cos(t0 * Math.PI/180) * oRadius)) 
-      +  "," + parseFloat(500 - (Math.sin(t0 * Math.PI/180) * oRadius))
-      + "L " + parseFloat(500 + (Math.cos(t0 * Math.PI/180) * iRadius)) 
-      + ", " + parseFloat(500 - (Math.sin(t0 * Math.PI/180) * iRadius))
-      + "A " + iRadius + "," + iRadius + " 1 " + arcSweep + " ,1 " 
-             + parseFloat(500 + (Math.cos(t1 * Math.PI/180) * iRadius)) 
-      +  "," + parseFloat(500 - (Math.sin(t1 * Math.PI/180) * iRadius))
-      + " Z");              
-      return path;
-    }
-    function bCircle (radius, fill) {
-      var circle = document.createElementNS("http://www.w3.org/2000/svg", 'circle');  // overlay
-      circle.setAttribute("cx", 500);
-      circle.setAttribute("cy", 500);
-      circle.setAttribute("r", radius);
-      circle.setAttribute("fill", fill);
-      return circle;
-    }  
-}     
+function calendarEntriesUI(calendarJSON) {
+  var gcSlots = JSON.parse(calendarJSON);
+  const oRadius = 500, iRadius = 400;
+  var zOffset = 0, zMinutes = 0, zh = 0, arcSweep = 0;
+  var zero = new Date(gcSlots.items[0].start.dateTime);
+  var duration = new Date(gcSlots.items[0].end.dateTime);
+  const diffSec = Date.now() - zero / 1000;
+  const days = parseInt(diffSec / (60 * 60 * 24));
+  const minutes = parseInt((diffSec - (days * 60 * 60 * 24)) / 60);
+  var counting =  "T - " + days + " days " + minutes + " minutes";  
+  zero.getHours() > 12 ? zh = zero.getHours() - 12 : zh = zero.getHours();
+  zOffset = 450 - ((zh * 30) + (zero.getMinutes() / 2));
+  zMinutes = duration / 60000 / 2;
+  (zMinutes > 180) ? arcSweep = 1 : arcSweep = 0;
+  svg.appendChild(bCircle(500, "#212121"));
+  svg.appendChild(pathPeriod(zOffset, zOffset - zMinutes, iRadius, oRadius, arcSweep, "#A00", 500));
+  svg.appendChild(bCircle(400, "#000"));
+  svg.appendChild(addText(244, "A00", 500, 360, parseInt(zero / 60000), 500));
+  svg.appendChild(addText(80, "#888", 500, 492, zero, 500));
+  return svg;
+  function addText(fType, fSize, color, x, y, title, fWeight) {
+    var text = document.createElementNS("http://www.w3.org/2000/svg", 'text');
+    text.setAttribute("font-size", fSize);
+    text.setAttribute("font-weight", fWeight);
+    text.setAttribute("fill", color);
+    text.setAttribute("x", x);
+    text.setAttribute("y", y);
+    text.textContent = title;
+    return text;
+  }
+  function pathPeriod(t0, t1, iRadius, oRadius, arcSweep, fill) {
+    var path = document.createElementNS("http://www.w3.org/2000/svg", 'path');
+    path.setAttribute("fill", fill);
+    path.setAttribute("stroke-width", 0);
+    path.setAttribute("d",
+      "M " + parseFloat(500 + (Math.cos(t1 * Math.PI / 180) * iRadius)) +
+      ", " + parseFloat(500 - (Math.sin(t1 * Math.PI / 180) * iRadius)) +
+      "L " + parseFloat(500 + (Math.cos(t1 * Math.PI / 180) * oRadius)) +
+      ", " + parseFloat(500 - (Math.sin(t1 * Math.PI / 180) * oRadius)) +
+      "A " + oRadius + "," + oRadius + " 0 " + arcSweep + " ,0 " +
+      parseFloat(500 + (Math.cos(t0 * Math.PI / 180) * oRadius)) +
+      "," + parseFloat(500 - (Math.sin(t0 * Math.PI / 180) * oRadius)) +
+      "L " + parseFloat(500 + (Math.cos(t0 * Math.PI / 180) * iRadius)) +
+      ", " + parseFloat(500 - (Math.sin(t0 * Math.PI / 180) * iRadius)) +
+      "A " + iRadius + "," + iRadius + " 1 " + arcSweep + " ,1 " +
+      parseFloat(500 + (Math.cos(t1 * Math.PI / 180) * iRadius)) +
+      "," + parseFloat(500 - (Math.sin(t1 * Math.PI / 180) * iRadius)) +
+      " Z");
+    return path;
+  }
+  function bCircle(radius, fill) {
+    var circle = document.createElementNS("http://www.w3.org/2000/svg", 'circle'); // overlay
+    circle.setAttribute("cx", 500);
+    circle.setAttribute("cy", 500);
+    circle.setAttribute("r", radius);
+    circle.setAttribute("fill", fill);
+    return circle;
+  }
+}
