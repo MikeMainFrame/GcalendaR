71d5a7baa88f4e8289bfacf5c78ccf577b5b6fe0
Michael Kenneth Rasch <miketriticum@gmail.com>
2019-02-16 07:36:28 +0100

    Update gc.js

--- gc.js
+++ gc.js
@@ -29,23 +29,35 @@ function calendarEntriesUI(calendarJSON) {
   var gcSlots = JSON.parse(calendarJSON);
   const oRadius = 500, iRadius = 400;
   var zOffset = 0, zMinutes = 0, zh = 0, arcSweep = 0;
-  var zero = new Date(gcSlots.items[0].start.dateTime);
-  var duration = new Date(gcSlots.items[0].end.dateTime);
-  const diffSec = Date.now() - zero / 1000;
-  const days = parseInt(diffSec / (60 * 60 * 24));
-  const minutes = parseInt((diffSec - (days * 60 * 60 * 24)) / 60);
-  var counting =  "T - " + days + " days " + minutes + " minutes";  
-  zero.getHours() > 12 ? zh = zero.getHours() - 12 : zh = zero.getHours();
-  zOffset = 450 - ((zh * 30) + (zero.getMinutes() / 2));
+  var t0 = new Date(gcSlots.items[0].start.dateTime);
+  const duration = (new Date(gcSlots.items[0].end.dateTime) - t0;
+  const tMinus = Date.now() - t0;
+  var days = parseInt(tMinus / (60 * 60 * 24));
+  var minutes = parseInt((tMinus - (days * 60 * 60 * 24)) / 60);
+  const counting =  "T - " + days + " days " + minutes + " minutes";  
+  t0.getHours() > 12 ? zh = t0.getHours() - 12 : zh = t0.getHours();
+  zOffset = 450 - ((zh * 30) + (t0.getMinutes() / 2));
   zMinutes = duration / 60000 / 2;
   (zMinutes > 180) ? arcSweep = 1 : arcSweep = 0;
   svg.appendChild(bCircle(500, "#212121"));
   svg.appendChild(pathPeriod(zOffset, zOffset - zMinutes, iRadius, oRadius, arcSweep, "#A00", 500));
   svg.appendChild(bCircle(400, "#000"));
-  svg.appendChild(addText(244, "A00", 500, 360, parseInt(zero / 60000), 500));
-  svg.appendChild(addText(80, "#888", 500, 492, zero, 500));
+  svg.appendChild(addText(244, "A00", 500, 360, parseInt(t0 / 60000), 500));
+  svg.appendChild(addText(80, "#888", 500, 492, t0, 500));
+  
+  setInterval(moveHands, 5000);   
+  
   return svg;
-  function addText(fType, fSize, color, x, y, title, fWeight) {
+  
+  function moveHands() {
+   var now = Date.now();
+   var diffSec = (t0 - now) / 1000;
+   var days = parseInt(diffSec / (60 * 60 * 24));
+   var minutes = parseInt((diffSec - (days * 60 * 60 * 24)) / 60);
+   document.getElementById("tMinus").textContent =  days + ":" + minutes;
+  }
+  
+  function addText(fSize, color, x, y, title, fWeight) {
     var text = document.createElementNS("http://www.w3.org/2000/svg", 'text');
     text.setAttribute("font-size", fSize);
     text.setAttribute("font-weight", fWeight);
