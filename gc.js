a03403b38772092d2f5cc248ac95c60d46fe9833
Michael Kenneth Rasch <miketriticum@gmail.com>
2019-03-14 14:03:08 +0100

    Update gc.js

--- gc.js
+++ gc.js
@@ -1,64 +1,115 @@
+var GoogleAuth;
+
 function handleClientLoad() {
   gapi.load('client:auth2', initClient);
 }
 function initClient() {
   gapi.client.init({
-    apiKey: '',
-    clientId: '',
+    apiKey: '........................',
+    clientId: '....................apps.googleusercontent.com',
     discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
     scope: 'https://www.googleapis.com/auth/calendar.readonly',
-  }).then(function() {
-    listUpcomingEvents()
+  }).then(function() {    
+    GoogleAuth = gapi.auth2.getAuthInstance();
+    GoogleAuth.isSignedIn.listen(listUpcomingEvents);
+    (GoogleAuth.isSignedIn.get()) 
+    ? listUpcomingEvents() 
+    : GoogleAuth.signIn();
   }, function(error) {
-    appendPre(JSON.stringify(error, null, 2), 'lColumn');
+    console.log(JSON.stringify(error, null));
   });
 }
 function listUpcomingEvents() {
   gapi.client.calendar.events.list({
     'calendarId': 'primary',
-    'timeMin': (new Date()).toISOString(),
+    'timeMax': (new Date()).toISOString(),
     'showDeleted': false,
     'singleEvents': true,
     'maxResults': 10,
     'orderBy': 'startTime'
   }).then(function(response) {
-    calendarEntriesUI(response);
+    var accumulated = 0;
+    for (var ix = 0; ix < response.result.items.length; ix++) {      
+      if (/#workhours/.test(response.result.items[ix].description)) {
+        document.body.appendChild(calendarEntriesUI(response.result.items[ix]))
+        var zero = new Date(response.result.items[ix].start.dateTime);
+        var plus = new Date(response.result.items[ix].end.dateTime);
+        accumulated = accumulated + ((plus - zero) / 6E4);
+      }
+    }
+    document.getElementById("zSum").textContent ="workhours found and accumulated: " + accumulated;
   });
 }
-function calendarEntriesUI(calendarJSON) {
-  var gcSlots = JSON.parse(calendarJSON);
-  const oRadius = 500, iRadius = 400;
+function calendarEntriesUI(gcSlots) {
+
+  const oRadius = 500, iRadius = 400; var daysMinutes = "Ø";
   var zOffset = 0, zMinutes = 0, zh = 0, arcSweep = 0;
-  var t0 = new Date(gcSlots.items[0].start.dateTime);
-  const duration = (new Date(gcSlots.items[0].end.dateTime) - t0;
-  const tMinus = Date.now() - t0;
-  var days = parseInt(tMinus / (60 * 60 * 24));
-  var minutes = parseInt((tMinus - (days * 60 * 60 * 24)) / 60);
-  const counting =  "T - " + days + " days " + minutes + " minutes";  
-  t0.getHours() > 12 ? zh = t0.getHours() - 12 : zh = t0.getHours();
-  zOffset = 450 - ((zh * 30) + (t0.getMinutes() / 2));
+  var zero = new Date(gcSlots.start.dateTime);
+  var duration = new Date(gcSlots.end.dateTime);  
+  var tminus = (Date.now() - zero) / 60000;
+  duration = duration - zero;
+  zero.getHours() > 12 ? zh = zero.getHours() - 12 : zh = zero.getHours();
+  zOffset = 450 - ((zh * 30) + (zero.getMinutes() / 2));
   zMinutes = duration / 60000 / 2;
   (zMinutes > 180) ? arcSweep = 1 : arcSweep = 0;
-  svg.appendChild(bCircle(500, "#212121"));
-  svg.appendChild(pathPeriod(zOffset, zOffset - zMinutes, iRadius, oRadius, arcSweep, "#A00", 500));
-  svg.appendChild(bCircle(400, "#000"));
-  svg.appendChild(addText(244, "A00", 500, 360, parseInt(t0 / 60000), 500));
-  svg.appendChild(addText(80, "#888", 500, 492, t0, 500));
+  (tminus < -1440) ? daysMinutes = parseInt(tminus / 1440) + " days" : daysMinutes = parseInt(tminus) + " minutes";
+
+  var svg = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
+  svg.setAttribute("width", "47%");
+  svg.setAttribute("viewBox", "0 0 2400 1200");
+  svg.setAttribute("style", "background: rgba(255,0,160,0.5) ; float:left; margin:1%");
+
+  var g = document.createElementNS("http://www.w3.org/2000/svg", 'g');
+
+  g.appendChild(bCircle(500, "#212121"));
+  g.appendChild(pathPeriod(zOffset, zOffset - zMinutes, iRadius, oRadius, arcSweep, "#F0A", 500));
+  g.appendChild(bCircle(400, "#000"));
+
+  var path = document.createElementNS("http://www.w3.org/2000/svg", 'path');
+  path.setAttribute("id", "ztext");  
+  path.setAttribute("fill", "none");
+  path.setAttribute("d", "M 1000,500 A 500,500 0 0,1 0,500");
+  g.appendChild(path);
+
+  var text = document.createElementNS("http://www.w3.org/2000/svg", 'text');
+  text.setAttribute("text-anchor", "start");
+  text.setAttribute("font-size", 64);
+  text.setAttribute("fill", "#fff");
+
+  var textPath = document.createElementNS("http://www.w3.org/2000/svg", 'textPath');
+  textPath.setAttribute("text-anchor", "start");
+  textPath.setAttribute("href", "#ztext");
+  textPath.textContent = daysMinutes;
+  text.appendChild(textPath);
+
+  g.appendChild(text);
+  g.appendChild(addText(144, "#F0A", 500, 360, zMinutes * 2, 500));
+  g.appendChild(addText(64, "#fff", 500, 492, gcSlots.start.dateTime, 100));
+
+  g.setAttribute("transform", "translate(100,100)");
+  svg.appendChild(g);
+
+  var temp = addText(96, "#fff", 1150, 200, gcSlots.summary, 900);
+  temp.setAttribute("text-anchor", "start");
+  svg.appendChild(temp);
   
-  setInterval(moveHands, 5000);   
-  
+  var fObject = document.createElementNS("http://www.w3.org/2000/svg", 'foreignObject');
+  fObject.setAttribute("x", 1200);
+  fObject.setAttribute("y", 400);
+  fObject.setAttribute("width", 800);
+  fObject.setAttribute("height", 800);
+
+  var div = document.createElementNS('http://www.w3.org/1999/xhtml', 'div');
+  div.textContent = gcSlots.description;
+  div.setAttribute("style", "font-size:36pt")
+  fObject.appendChild(div);
+  svg.appendChild(fObject);
+
   return svg;
-  
-  function moveHands() {
-   var now = Date.now();
-   var diffSec = (t0 - now) / 1000;
-   var days = parseInt(diffSec / (60 * 60 * 24));
-   var minutes = parseInt((diffSec - (days * 60 * 60 * 24)) / 60);
-   document.getElementById("tMinus").textContent =  days + ":" + minutes;
-  }
-  
+
   function addText(fSize, color, x, y, title, fWeight) {
     var text = document.createElementNS("http://www.w3.org/2000/svg", 'text');
+    text.setAttribute("text-anchor", "middle");
     text.setAttribute("font-size", fSize);
     text.setAttribute("font-weight", fWeight);
     text.setAttribute("fill", color);
